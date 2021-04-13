8bd4ef1492c4b468c90d2a4e1c27cf98e2bfdd33
Nicholas Livingstone <nlivingsto@unm.edu>
2021-04-12 17:47:02 -0700

    Integrated player into the world and added transitions between scenes
    Player prefab added to the world. Can be controlled as expected. Additionally added scene transitions when entering and existing tower.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -16,25 +17,44 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 velocity;
     private bool isGrounded;
 
+
+
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if (isGrounded && velocity.y < 0)
+        if (groundCheck != null)
         {
-            velocity.y = -2f;
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
         }
+    }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player entered triggered with:" + other);
 
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
+        if (other.gameObject.CompareTag("CastleEntrance"))
+        {
+            SceneManager.LoadSceneAsync("Tower Interior");
+        }
+        else if (other.gameObject.CompareTag("InteriorDoor"))
+        {
+            SceneManager.LoadSceneAsync("Esotyre");
+        }
     }
 }
