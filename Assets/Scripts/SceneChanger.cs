cc88bc4fff2248f02d03c198119bde3fbe2749e8
Clarissa Garcia <cgarcia528@unm.edu>
2021-04-20 12:48:13 -0600

    Was able to make inventory persistent to TowerInside

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -5,8 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-	public void LoadScene(string sceneName)
-	{
-		SceneManager.LoadScene(""+sceneName);
-	}
+    public GameObject playerInventoryCanvas;
+    public string nextScene;
+
+
+    public void clickButton() {
+        StartCoroutine(LoadYourAsyncScene(nextScene));
+    }
+    public IEnumerator LoadYourAsyncScene(string nextScene)
+    {
+        Debug.Log("Been called");
+        Debug.Log(nextScene);
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(""+nextScene, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(playerInventoryCanvas, SceneManager.GetSceneByName(""+nextScene));
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
 }
