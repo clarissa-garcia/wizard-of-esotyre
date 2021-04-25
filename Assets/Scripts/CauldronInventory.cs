1052a7cb510b3538ed94c4685eca63411a01b04d
cgarcia528 <cgarcia528@unm.edu>
2021-04-24 19:34:18 -0500

    Added cauldron inventory by using an empty game object in the hierarchy. If an inventory object is clicked in the workstation scene, then it is detracted from their inventory and added to the cauldron object

---
+++ Assets/Scripts/CauldronInventory.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CauldronInventory : MonoBehaviour
+{
+    public Item[] cauldronInventory = new Item[7];
+
+    // Update is called once per frame
+    public void addItem(Item newItem) {
+        for (int i = 0; i < cauldronInventory.Length; i++) {
+            if (cauldronInventory[i] == null) {
+                cauldronInventory[i] = newItem;
+                printCauldronInventory();
+                return;
+            }
+        }
+    }
+
+    public void printCauldronInventory() {
+        Debug.Log("Start of inventory:");
+        for (int i = 0; i < cauldronInventory.Length; i++)
+        {
+            if (cauldronInventory[i] != null) {
+                Debug.Log(cauldronInventory[i].itemName);
+            }
+        }
+        Debug.Log("End of inventory");
+    }
+}
