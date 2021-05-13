60a33417677c53b3f4624967a08935feb0a63038
Nicholas Livingstone <nlivingsto@unm.edu>
2021-05-13 13:21:13 -0600

    Created framework for bridge repair. Debug setup to repair bridge.

---
+++ Assets/BridgeManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BridgeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject repairedBridge;
+    public GameObject brokenBridge; 
+
+    void Start()
+    {
+        GameManager.OnBridgeRepair += OnBridgeRepair;
+        if(GameManager.Instance.BridgeRepaired()){
+            repairedBridge.SetActive(true);
+            brokenBridge.SetActive(false);
+        }
+        else{
+            repairedBridge.SetActive(false);
+            brokenBridge.SetActive(true);
+        }
+    }
+
+    public void OnBridgeRepair(){
+        repairedBridge.SetActive(true);
+        brokenBridge.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using DialogueEditor;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Where the player is at in terms of progression in the world
+/// </summary>
+public enum GameState
+{
+    TOWER,
+    TOWN,
+    FOREST,
+    CAVE
+}
+
+public delegate void OnStateChangeHandler();
+public delegate void OnBridgeRepairHandler();
+
+public class GameManager : MonoBehaviour
+{
+    protected GameManager() { }
+    public  static GameManager Instance = null;
+    public event OnStateChangeHandler OnStateChange;
+    public static event OnBridgeRepairHandler OnBridgeRepair;
+    public GameState gameState { get; private set; }
+
+    private static bool bridgeRepaired = false; 
+
+    private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+    
+
+
+    public bool BridgeRepaired(){
+        return bridgeRepaired;
+    }
+
+    public void FixBridge(){
+        bridgeRepaired = true; 
+        Debug.Log("Bridge Repaired");
+        OnBridgeRepair();
+    }
+
+    public void SetGameState(GameState state)
+    {
+        this.gameState = state;
+        OnStateChange();
+    }
+
+    public void OnApplicationQuit()
+    {
+        GameManager.Instance = null; 
+    }
+
+}
