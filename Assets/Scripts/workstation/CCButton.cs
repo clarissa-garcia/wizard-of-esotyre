d1168705d3e423fa385923c6f3439141cab3ae99
Preston Hamilton <pham7@unm.edu>
2021-05-14 04:35:00 -0700

    Fixing CCW button

--- Assets/Scripts/workstation/CCButton.cs
+++ Assets/Scripts/workstation/CCButton.cs
@@ -5,25 +5,26 @@ using UnityEngine.UI;
 
 public class CCButton : MonoBehaviour
 {
-	private Button directionB;
-	private StirCounter counter;
-	
+    public Button directionB;
+    private StirCounter counter;
+    
     // Start is called before the first frame update
     void Start()
     {
-		directionB = GameObject.Find("Direction Button").GetComponent<Button>();
+        directionB = GetComponent<Button>();
+        if(directionB == null) Debug.Log("direction null");
         counter = GameObject.Find("Stir Count").GetComponent<StirCounter>();
-		//directionB.onClick.AddListener(directionSwitch);
-	}
+        directionB.onClick.AddListener(directionSwitch);
+    }
 
     // Update is called once per frame
     void Update()
     {
         
     }
-	
-	public void directionSwitch(){
-		Debug.Log("button clicked");
-		counter.flipDirection();
-	}
+    
+    public void directionSwitch(){
+        Debug.Log("button clicked");
+        counter.flipDirection();
+    }
 }
